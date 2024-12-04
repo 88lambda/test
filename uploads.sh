dfab85eb509f869210f40a620cd934122f2fdd48
Egor Batsin <egor.batsin@onlyoffice.com>
2024-12-04 10:55:14 +0300

    Update uploads.sh

--- uploads.sh
+++ uploads.sh
@@ -6,7 +6,7 @@ DIR_DE="onlyoffice-ds-integration"
 generate_command() {
   local dir=$1
   local files=("$dir"/*) 
-  local command="./univention-appcenter-control upload --username ${{ secrets.PORTAL_USER }} --pwdfile ./pwdfile --noninteractive 5.0/onlyoffice-ds=${{ inputs.ver }} \\"
+  local command="./univention-appcenter-control upload --username ./username --pwdfile ./pwdfile --noninteractive 5.0/onlyoffice-ds=${{ inputs.ver }} \\"
   
   for file in "${files[@]}"; do
     command+="\n           $file \\"
