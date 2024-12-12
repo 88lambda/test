9463c0335d5e371f93c4899f1b5ae302efc97e19
Egor Batsin <egor.batsin@onlyoffice.com>
2024-12-12 16:52:13 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -21,7 +21,7 @@ version=$app_ver
 sed -i 's|appversion|$version|' "$app_ini"
 sed -i 's|imageversion|$version|' "$app_ini"
 
-$appcenterctl new-version $credentials $ucsver/$app_name $ucsver/$app_name=$version
+echo -e $appcenterctl new-version $credentials $ucsver/$app_name $ucsver/$app_name=$version
 
 file_list=$(ls)
 
