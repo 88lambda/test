5b0126f15c04d06eac478f2c3235724c17ed3ceb
Egor Batsin <egor.batsin@onlyoffice.com>
2024-12-12 18:38:20 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -30,4 +30,4 @@ echo -e $appcenterctl new-version $credentials $ucsver/$app_name $ucsver/$app_na
 file_list=$(ls)
 common_files=$(ls "../../common")
 
-$appcenterctl upload $credentials --noninteractive $ucsver/$app_name=$version $file_list $common_files
+$appcenterctl upload $credentials --noninteractive $ucsver/$app_name=$version $common_files $file_list
