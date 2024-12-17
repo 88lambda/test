a14da94b40e447dd16ab6e55efe74dbad859de1c
Egor Batsin <egor.batsin@onlyoffice.com>
2024-12-17 13:35:26 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -23,7 +23,7 @@ sed -i "s|imageversion|$app_ver|" "$app_ini"
 $appcenterctl new-version $credentials $ucsver/$app_name $ucsver/$app_name=$version
 
 file_list=$(ls)
-common_files=$(find "../../common" -type f -exec realpath {} \;)
+#common_files=$(find "../../common" -type f -exec realpath {} \;)
+common_files=$(ln -s "../../common" "/$app_name")
 
 $appcenterctl upload $credentials --noninteractive $ucsver/$app_name=$version $file_list $common_files
-
