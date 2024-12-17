4835faf8ce9d26a9f9c7225031f25b974a3114c3
Egor Batsin <egor.batsin@onlyoffice.com>
2024-12-17 13:36:31 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ fi
 sed -i "s|appversion|$app_ver|" "$app_ini"
 sed -i "s|imageversion|$app_ver|" "$app_ini"
 
-$appcenterctl new-version $credentials $ucsver/$app_name $ucsver/$app_name=$version
+echo -e $appcenterctl new-version $credentials $ucsver/$app_name $ucsver/$app_name=$version
 
 file_list=$(ls)
 #common_files=$(find "../../common" -type f -exec realpath {} \;)
