bb8725d8791c6e38d588aa7c60fb11e169977dd9
Egor Batsin <egor.batsin@onlyoffice.com>
2024-12-24 13:45:41 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ fi
 sed -i "s|appversion|$app_ver|" "$app_ini"
 sed -i "s|imageversion|$app_ver|" "$app_ini"
 
-$appcenterctl new-version --username $username --pwdfile $passfile $ucsver/$app_name $ucsver/$app_name=$app_ver
+echo -e $appcenterctl new-version --username $username --pwdfile $passfile $ucsver/$app_name $ucsver/$app_name=$app_ver
 
 file_list=$(ls)
 
